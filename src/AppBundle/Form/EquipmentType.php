74b2da8694cf938fb6b11f8c7b3293f4557bf1ba
Clement PAQUIN <clement.paquin@gmail.com>
2018-05-25 10:50:51 +0200

    space after coma for travis

--- src/AppBundle/Form/EquipmentType.php
+++ src/AppBundle/Form/EquipmentType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,7 @@ class EquipmentType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, array('label'=> 'Nom de l\'équipement'))
-            ->add('description', TextType::class, array('label'=> 'Description de l\'équipement'));
+            ->add('description', TextareaType::class, array('label'=> 'Description de l\'équipement'));
     }
     
     /**
