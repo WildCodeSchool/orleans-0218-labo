5325a2df1f4efa68fea4cd73e6bedce5bac8fc89
Clement PAQUIN <clement.paquin@gmail.com>
2018-05-25 11:41:27 +0200

    Edit'part done, SCSS ok

--- src/AppBundle/Form/EquipmentType.php
+++ src/AppBundle/Form/EquipmentType.php
@@ -17,8 +17,10 @@ class EquipmentType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, array('label'=> 'Nom de l\'équipement'))
-            ->add('description', TextareaType::class,
-                array('label'=> 'Description de l\'équipement', 'attr' => array('cols' => '5', 'rows' => '5')));
+            ->add(
+                'description', TextareaType::class, array('label'=> 'Description de l\'équipement',
+                'attr' => array('cols' => '5', 'rows' => '5'))
+            );
     }
     
     /**
