dc7d0e6299632f9542680754d345b20df9717230
Clement PAQUIN <clement.paquin@gmail.com>
2018-06-05 16:53:58 +0200

    Fix

--- src/AppBundle/Controller/EquipmentController.php
+++ src/AppBundle/Controller/EquipmentController.php
@@ -18,7 +18,7 @@ class EquipmentController extends Controller
      * Lists all Equipment entities.
      *
      * @Route("/",    name="equipment_index")
-     * @Method({"GET"})
+     * @Method("GET")
      */
     public function indexAction()
     {
