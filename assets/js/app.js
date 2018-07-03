e2c5b95c4fc0f9a862d59c0f405bdf612d088967
Julia Limousin <julia.limousin2@gmail.com>
2018-07-03 10:56:43 +0200

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
 import $ from "jquery";
+import "bootstrap-sass/assets/javascripts/bootstrap";
+import "bootstrap-sass/assets/javascripts/bootstrap.min";
+import "bootstrap-sass/assets/javascripts/bootstrap/modal";
 
 $(document).ready(function () {
+    $("#button-modal").click(function () {
+        $("#myModal").modal('show');
+    });
 });
