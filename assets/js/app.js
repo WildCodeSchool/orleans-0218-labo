e414064ea95baae2c55aa9d763f782dc926e07d2
Anthony Sottejeau <anthony.sottejeau@gmail.com>
2018-07-03 14:16:32 +0200

    affichage de la premiere signature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 import $ from "jquery";
+import "bootstrap-sass/assets/javascripts/bootstrap";
+import "bootstrap-sass/assets/javascripts/bootstrap.min";
+import "bootstrap-sass/assets/javascripts/bootstrap/modal";
 
 $(document).ready(function () {
+    $("#button-modal").click(function () {
+        console.log('test');
+        $("#myModal").modal('show');
+    });
 });
